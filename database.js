5a4e15647ef9a14c0cac80fa93502aeb4426d110
Agusmac <agusmac40@gmail.com>
2021-09-13 15:25:44 -0300

    F?

--- database.js
+++ database.js
@@ -2,35 +2,35 @@ require('dotenv').config()
 const mysql = require("mysql");
 
 
-const {pass,host,user,database}=process.env
+const { pass, host, user, database } = process.env
 
 // console.log({pass,host,user,database})
 
 const config = {
-  host: host,
-  user: user,
-  password:pass,
-  database: database,
+    host: host,
+    user: user,
+    password: pass,
+    database: database,
 };
 
 
 const pool = mysql.createPool(config);
 
-pool.getConnection((err,connection) =>{
-    if(err){
-        if(err.code === "PROTOCOL_CONNECTION_LOST"){
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
             console.error("DATABASE CONNECTION WAS CLOSED");
         }
-        if(err.code === "ER_CON_COUNT_ERROR"){
+        if (err.code === "ER_CON_COUNT_ERROR") {
             console.error("DATABASE HAS TOO MANY CONNECTIONS");
         }
-        if(err.code === "ECONREFUSED"){
+        if (err.code === "ECONREFUSED") {
             console.error("DATABASE CONNECTION WAS REFUSED");
         }
-        
+
     }
-    if(connection)connection.release();
-    
+    if (connection) connection.release();
+
     console.log("DB is Connected");
     return
 });
